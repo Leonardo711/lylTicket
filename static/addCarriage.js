4d0478a81efac7cf427e35c767101edfc9d614e7
leo <leo@gmail.com>
2016-12-12 00:41:41 +0800

    js fix

--- static/addCarriage.js
+++ static/addCarriage.js
@@ -46,7 +46,6 @@ $(document).ready(function(){
 
             $(row).children().children().children().each(function () {
                 updateElementIndex(this, prefix, formCount);
-                $(this).val("");
                 if ($(this).attr("readonly")=="readonly"){
                     $(this).val(formCount+1);
                 }
